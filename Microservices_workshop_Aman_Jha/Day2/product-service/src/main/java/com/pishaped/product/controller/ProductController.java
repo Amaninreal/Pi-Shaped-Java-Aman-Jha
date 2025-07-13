0c3d0a17828f2be765b674dc9d8e136305add482
Amaninreal <amaninreallife03@gmail.com>
2025-07-14 03:21:22 +0530

    adding config-server & product service with routing

---
+++ Microservices_workshop_Aman_Jha/Day2/product-service/src/main/java/com/pishaped/product/controller/ProductController.java
@@ -0,0 +1,18 @@
+package com.pishaped.product.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class ProductController {
+    @Value("${some.dynamic.property:Default Value}")
+    private String dynamicMessage;
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return "Product Service Message: " + dynamicMessage;
+    }
+}
